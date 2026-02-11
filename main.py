9e16437ec6bf86892f09974db53b0710bf86a3b1
Sadhika Saji <sdhksaji@gmail.com>
2026-02-11 11:48:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from student import PythonAnalysis
+from management import management
+
+name_obj=PythonAnalysis()
+dept_obj=management()
+
+name_obj.gouri()
+name_obj.alfia()
+name_obj.sadhika()
+name_obj.ashlin()
+name_obj.rayan()
+
+dept_obj.cs()
+dept_obj.ai()
+dept_obj.mech()
+dept_obj.civil()
+dept_obj.ec()
---
+++ management.py
@@ -0,0 +1,11 @@
+class management:
+    def cs(self):
+        print("this is CS department")
+    def ai(self):
+        print("this is AI department")
+    def mech(self):
+        print("this is mechanical department")
+    def civil(self):
+        print("this is civil department")
+    def ec(self):
+        print("this is electronic department")
